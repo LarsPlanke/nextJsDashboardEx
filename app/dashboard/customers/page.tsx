18e9d120a323161eb83feb74ae46ed57aaf635a3
Lars Planke <planke93@hotmail.com>
2025-03-17 11:09:17 +0100

    adding meta things

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Costumers',
+  };
+
+
+
 export default function Page() {
     return <p> costumers Page </p>;
 }
