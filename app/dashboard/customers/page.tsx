3e0258e23aaf08b51c773ee8ccd1d3d3410ea8f0
Lars Planke <planke93@hotmail.com>
2025-03-20 09:08:47 +0100

    add costumer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,27 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CostumersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
 
+
 export const metadata: Metadata = {
-    title: 'Costumers',
+    title: 'Customers',
   };
 
 
-
-export default function Page() {
-    return <p> costumers Page </p>;
+export default async function Page(props: { 
+    searchParams: Promise<{ 
+        query?: string;
+        page?: string;
+     }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams.query || '';
+    
+    const customers = await fetchFilteredCustomers(query);
+    
+    return (
+        <main>
+            <CostumersTable customers={customers} /> 
+        </main>
+    );
 }
